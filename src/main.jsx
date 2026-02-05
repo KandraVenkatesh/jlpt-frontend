8ec23c66a387c8551ae96c8065f22d9d4290d336
KandraVenkatesh <vchowdhary753@gmail.com>
2026-02-05 17:46:25 +0530

    Fix case-sensitive CSS import for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './styles/home.css';
-import './components/about.css';
+import './components/About.css';
 
 
 
